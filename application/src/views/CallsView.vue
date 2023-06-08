855e614ed1238a44db78b7fbf26e41e1b3c703cb
Mike Roguez <mike.roguez@gmail.com>
2023-06-08 12:44:13 -0600

    feat(Content): New call for poster track

--- application/src/views/CallsView.vue
+++ application/src/views/CallsView.vue
@@ -21,19 +21,25 @@ import OrganizerItem from '@/components/OrganizerItem.vue';
                 <div class="ms-2 me-auto">
                   <div class="fw-bold"><a href="./call-for-participation#cfp-call" class="uline">Call for papers
                       (CFP)</a></div>
-                  <strong class="text-primary text-gradient">Deadline for Abstract submission:</strong> June 12th 
+                  <strong class="text-primary text-gradient">Deadline for Abstract submission:</strong> June 12th
                   <br><a class="uline" href="./call-for-participation#cpf-dates">See all CFP important dates</a>
                 </div>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
-                  <div class="fw-bold">Graduate Colloquium (GC)</div>
-                  <strong class="text-primary text-gradient">Deadline for submission:</strong> Pending<br>
+                  <div class="fw-bold">
+                    <a href="./call-for-participation#cpt-call" class="uline">
+                      Call for poster track (CPT)
+                    </a>
+                  </div>
+                  <strong class="text-primary text-gradient">Deadline for submission:</strong>
+                  July 31th<br>
+                  <a class="uline" href="./call-for-participation#cpt-dates">See all CPT important dates</a>
                 </div>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
-                  <div class="fw-bold">Poster Track (PT)</div>
+                  <div class="fw-bold">Graduate Colloquium (GC)</div>
                   <strong class="text-primary text-gradient">Deadline for submission:</strong> Pending<br>
                 </div>
               </li>
@@ -127,7 +133,7 @@ import OrganizerItem from '@/components/OrganizerItem.vue';
 
             <p>
               Submission format to be used for each category will follow the <a
-                href="https://www.acm.org/publications/proceedings-template" class="uline">new ACM Master Article 
+                href="https://www.acm.org/publications/proceedings-template" class="uline">new ACM Master Article
                 format</a>. Authors may
               choose to work in one of the following formats:
             </p>
@@ -162,8 +168,11 @@ import OrganizerItem from '@/components/OrganizerItem.vue';
             </p>
 
             <p>
-              We are working to ensure that accepted papers will be published in the ACM Digital Library. 
-              In addition, best papers will be recommended for submitting an extended version and be published in a special edition book of the prestigious <a href="https://www.springer.com/series/6033" class="uline">Human–Computer Interaction Series</a>, published by Springer and regularly indexed in the master book list of the Web of Science.
+              We are working to ensure that accepted papers will be published in the ACM Digital Library.
+              In addition, best papers will be recommended for submitting an extended version and be published in a
+              special edition book of the prestigious <a href="https://www.springer.com/series/6033"
+                class="uline">Human–Computer Interaction Series</a>, published by Springer and regularly indexed in the
+              master book list of the Web of Science.
             </p>
 
             <h3 class="my-4">Program (Paper) Chairs</h3>
@@ -194,7 +203,8 @@ import OrganizerItem from '@/components/OrganizerItem.vue';
                     <div class="d-block blur-shadow-image">
                       <img src="/assets/img/people/vanessa.png" alt="" class="img-fluid shadow rounded-3">
                     </div>
-                    <div class="colored-shadow" style="background-image: url(&quot;./assets/img/people/vanessa.png&quot;);">
+                    <div class="colored-shadow"
+                      style="background-image: url(&quot;./assets/img/people/vanessa.png&quot;);">
                     </div>
                   </template>
                   <template #name>
@@ -207,17 +217,142 @@ import OrganizerItem from '@/components/OrganizerItem.vue';
                   </template>
                 </OrganizerItem>
               </div>
+
             </div>
             <br>
             <p><a class="uline" href="./call-for-participation#top">Back to top of page</a></p>
             <!-- end call for papers -->
+
+            <!-- Poster track-->
+            <hr>
+            <h2 id="cpt-call" class="text-primary text-gradient text-center py-4">Call for poster track</h2>
+            <p>
+              We invite practitioners, researchers, and students to submit their work to the poster track at CLIHC 2023.
+              Posters provide a great opportunity for academics and practitioners to get valuable feedback on early-stage
+              work and discuss potential collaborations. Posters must present work-in-progress, preliminary results,
+              small-scale studies, late-breaking scientific and professional news showing promise, a demonstration of a
+              technical tool, etc. Contributions will be exhibited in a poster session during the CLIHC conference. Papers
+              submitted to the poster track can be written either in English, Spanish, or Portuguese and must be written
+              in the form of self-contained short research papers.
+            </p>
+
+            <h3 id="cpt-dates">Important dates (posters)</h3>
+            <ul>
+              <li>Submission of poster papers: July 31th</li>
+              <li>Notification: August 28th</li>
+              <li>Camera ready: September 14th</li>
+              <li>Event Oct 30 - 31, Nov 1</li>
+            </ul>
+
+            <h3>Submission details</h3>
+
+            <P>
+              Authors wishing to submit a paper must:
+            </P>
+            <ul>
+              <li>
+                Submit a 4-page paper using the <a href="https://www.acm.org/publications/proceedings-template"
+                  class="uline">new ACM Master Article format</a>. Authors may choose to work in one of the
+                following formats:
+                <ul>
+                  <li>Microsoft Word</li>
+                  <li>LaTeX (recommended)</li>
+                </ul>
+              </li>
+              <li>
+                All materials must be submitted electronically to the <a
+                  href="https://easychair.org/conferences/?conf=clihc2023" class="uline">EasyChair Conference System</a>,
+                to the Workshop and
+                Tutorials’ track, by the deadline.
+              </li>
+            </ul>
+            <p>
+              Your submission must be original work. It cannot be under concurrent review for publication by another
+              conference or journal nor has been published elsewhere. Please submit accurate and complete references.
+            </p>
+            <p>
+              Accepted poster can choose one of the following publication media:
+            </p>
+            <ul>
+              <li>Conference Proceedings published in ACM DL. Only English submissions can be published in this medium. If
+                your submission was accepted in Spanish or Portuguese and wish to publish here, you will have to translate
+                your original submission to English.</li>
+              <li>Avances en Interacción Humano-Computadora. Only Spanish or English submissions can be published in this
+                medium. If your submission was accepted in Portuguese and wish to publish here, you will have to translate
+                your original submission to either Spanish or English.</li>
+            </ul>
+
+            <h3>Review process</h3>
+            <p>
+              Papers will be blind peer-reviewed by members of the CLIHC 2023 Poster Track Program Subcommittee (TBD).
+              Submissions will be evaluated based on their originality, significance of the contribution to the field,
+              technical correctness, and presentation.
+            </p>
+            <p>
+              The Poster chairs will send poster exhibition instructions to the authors of the accepted papers.
+            </p>
+
+            <h3>Attendance</h3>
+            <p>
+              One author of each accepted submission must register for the conference and present the poster, with a
+              preference for in-person presentation. However, virtual presentation options may be considered.
+            </p>
+
+            <h3 class="my-4">Posters Chairs</h3>
+            <div class="row">
+              <div class="col-md-4">
+                <OrganizerItem>
+                  <template #image>
+                    <div class="d-block blur-shadow-image">
+                      <img src="/assets/img/people/jaime.png" alt="" class="img-fluid shadow rounded-3">
+                    </div>
+                    <div class="colored-shadow" style="background-image: url(&quot;./assets/img/people/jaime.png&quot;);">
+                    </div>
+                  </template>
+                  <template #name>
+                    Jaime Muñoz Arteaga
+                  </template>
+                  <template #institution>
+                    U. Aguascalientes, MX
+                  </template>
+                  <template #email>
+                  </template>
+                </OrganizerItem>
+              </div>
+
+              <div class="col-md-4">
+                <OrganizerItem>
+                  <template #image>
+                    <div class="d-block blur-shadow-image">
+                      <img src="/assets/img/people/adriana.png" alt="" class="img-fluid shadow rounded-3">
+                    </div>
+                    <div class="colored-shadow"
+                      style="background-image: url(&quot;./assets/img/people/adriana.png&quot;);">
+                    </div>
+                  </template>
+                  <template #name>
+                    Adriana Iñiguez
+                  </template>
+                  <template #institution>
+                    Universidad de Guadalajara, MX
+                  </template>
+                  <template #email>
+                  </template>
+                </OrganizerItem>
+              </div>   
+            </div>
+            <br>
+            <p><a class="uline" href="./call-for-participation#top">Back to top of page</a></p>                             
+            <!-- end Poster tracks -->
           </div>
         </div>
       </div>
     </div>
-  </section></template>
+  </section>
+</template>
 
-<style>dt {
+<style>
+dt {
   display: list-item;
   list-style-type: disc;
   list-style-position: inside;
