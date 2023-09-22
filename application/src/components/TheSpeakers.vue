37db73970055f87e44fd9f4e02529b8355870581
Mike Roguez <mike.roguez@gmail.com>
2023-09-22 00:20:15 -0600

    feat: The speakers en el home

--- application/src/components/TheSpeakers.vue
+++ application/src/components/TheSpeakers.vue
@@ -1,5 +1,37 @@
-<script setup>
-import Speakers from './locales/en/Speakers-small.vue'
+<script>
+import { shallowRef, watch, defineAsyncComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export default {
+  setup() {
+  
+    const { locale } = useI18n()
+    const contentComponent = shallowRef(null)
+
+    const loadComponents = async (newLocale) => {
+      contentComponent.value = await getContentForLocale(newLocale)
+    }
+
+    watch(locale, loadComponents, { immediate: true })
+
+    async function getContentForLocale(locale) {
+      switch (locale) {
+        case 'en':
+          return defineAsyncComponent(() => import('@/components/locales/en/Speakers-small.vue'))
+        case 'es':
+          return defineAsyncComponent(() => import('@/components/locales/es/Speakers-small.vue'))
+        case 'pt':
+          return defineAsyncComponent(() => import('@/components/locales/pt/Speakers-small.vue'))
+        default:
+          return null
+      }
+    }
+
+    return {
+      contentComponent,
+    }
+  }
+}
 </script>
 
 <template>
@@ -15,7 +47,9 @@ import Speakers from './locales/en/Speakers-small.vue'
                     </div>
                 </div>
                 <div class="col-lg-9">                    
-                    <Speakers />
+                    <template v-if="contentComponent">
+                        <component :is="contentComponent" />
+                    </template>
                 </div>
             </div>
         </div>
