e49647eb096de70db203a57b5da7f7973b1d7773
Mike Roguez <mike.roguez@gmail.com>
2023-11-01 11:50:46 -0600

    feat(Content): Agrega sedes virtuales en el home

--- application/src/components/SponsorItem.vue
+++ application/src/components/SponsorItem.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- ms-auto -->
-    <div class="col-lg-4 mt-lg-0 mt-4">
+    <div class="col-lg-4 mt-lg-0 mt-4 mb-2">
         <div class="card shadow-lg">
             <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
                 <div class="d-block blur-shadow-image">
---
+++ application/src/components/VirtualVenues.vue
@@ -0,0 +1,112 @@
+<script>
+import Tr from '@/i18n/translation'
+import SponsorItem from '@/components/SponsorItem.vue'
+
+export default {
+    components: {
+        SponsorItem
+    },    
+    setup() {
+        return { Tr }
+    }
+}
+</script>
+<template>
+    <section>
+        <div class="container mt-5" id="specialThanks">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="position-sticky pb-lg-5 pb-3 ps-2" style="top: 100px">
+                        <h3>{{ $t("home.virtual_venues") }}</h3>
+                        <p class="text-secondary font-weight-normal pe-3">
+                            {{ $t("home.virtual_venues_message") }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="row">                      
+
+                        <SponsorItem>
+                            <template #image>
+                                <img src="/assets/img/logos/itson.png" alt="" class="img-fluid shadow rounded-3">
+                            </template>
+                            <template #image-shadow>
+                                <div class="colored-shadow"
+                                    style="background-image: url(&quot;./assets/img/logos/itson.png&quot;);">
+                                </div>
+                            </template>                                
+                            <template #name>
+                            </template>
+                        </SponsorItem> 
+
+                        <SponsorItem>
+                            <template #image>
+                                <img src="/assets/img/logos/uabc.png" alt="" class="img-fluid shadow rounded-3">
+                            </template>
+                            <template #image-shadow>
+                                <div class="colored-shadow"
+                                    style="background-image: url(&quot;./assets/img/logos/uabc.png&quot;);">
+                                </div>
+                            </template>                                
+                            <template #name>
+                            </template>
+                        </SponsorItem>
+
+                        <SponsorItem>
+                            <template #image>
+                                <img src="/assets/img/logos/uaslp.png" alt="" class="img-fluid shadow rounded-3">
+                            </template>
+                            <template #image-shadow>
+                                <div class="colored-shadow"
+                                    style="background-image: url(&quot;./assets/img/logos/uaslp.png&quot;);">
+                                </div>
+                            </template>                                
+                            <template #name>
+                            </template>
+                        </SponsorItem>
+
+                        <SponsorItem>
+                            <template #image>
+                                <img src="/assets/img/logos/uap.png" alt="" class="img-fluid shadow rounded-3">
+                            </template>
+                            <template #image-shadow>
+                                <div class="colored-shadow"
+                                    style="background-image: url(&quot;./assets/img/logos/uap.png&quot;);">
+                                </div>
+                            </template>                                
+                            <template #name>
+                            </template>
+                        </SponsorItem>
+
+                        <SponsorItem>
+                            <template #image>
+                                <img src="/assets/img/logos/unca.png" alt="" class="img-fluid shadow rounded-3">
+                            </template>
+                            <template #image-shadow>
+                                <div class="colored-shadow"
+                                    style="background-image: url(&quot;./assets/img/logos/unca.png&quot;);">
+                                </div>
+                            </template>                                
+                            <template #name>
+                            </template>
+                        </SponsorItem>
+
+                        <SponsorItem>
+                            <template #image>
+                                <img src="/assets/img/logos/upp.png" alt="" class="img-fluid shadow rounded-3">
+                            </template>
+                            <template #image-shadow>
+                                <div class="colored-shadow"
+                                    style="background-image: url(&quot;./assets/img/logos/upp.png&quot;);">
+                                </div>
+                            </template>                                
+                            <template #name>
+                            </template>
+                        </SponsorItem>                        
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
